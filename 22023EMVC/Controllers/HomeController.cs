2fb5e307ac55822c5c6d6512fce4ef096b47a8a9
OnEAbOvEaLI <sudarshangirix789@gmail.com>
2025-01-07 11:03:06 +0545

    commit18

--- 22023EMVC/Controllers/HomeController.cs
+++ 22023EMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Security.Claims;
 using _22023EMVC.Data;
 using _22023EMVC.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -40,7 +41,49 @@ namespace _22023EMVC.Controllers
             }
             return View(model);
         }
-      
+        [HttpGet("SendMessageToAll")]
+        [Authorize]
+        public async Task<IActionResult> SendMessageToAll(string user, string message)
+        {
+            await _basicChatHub.Clients.All.SendAsync("MessageReceived", user, message);
+            return Ok();
+        }
+        [HttpGet("SendMessageToReceiver")]
+        [Authorize]
+        public async Task<IActionResult> SendMessageToReceiver(string sender, string receiver, string message)
+        {
+            var userId = _db.Users.FirstOrDefault(u => u.Email.ToLower() == receiver.ToLower())?.Id;
+
+            if (!string.IsNullOrEmpty(userId))
+            {
+                await _basicChatHub.Clients.User(userId).SendAsync("MessageReceived", sender, message);
+            }
+            return Ok();
+        }
+        [HttpGet("SendMessageToGroup")]
+        [Authorize]
+        public async Task SendMessageToGroup(string message)
+        {
+            var user = GetUserId();
+            var role = (await GetUserRoles(user)).FirstOrDefault();
+            var username = _db.Users.FirstOrDefault(u => u.Id == user)?.Email ?? "";
+            if (!string.IsNullOrEmpty(role))
+            {
+                await _basicChatHub.Clients.Group(role).SendAsync("MessageReceived", username, message);
+            }
+        }
+        private string GetUserId()
+        {
+            var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return userId;
+        }
+        private async Task<IList<string>> GetUserRoles(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            var roles = await _userManager.GetRolesAsync(user);
+            return roles;
+        }
+
         [Authorize(Roles ="Admin")]
         public IActionResult Privacy()
         {
